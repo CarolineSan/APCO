71bb87b1b896ae13b548eda3cd646ed5df39b439
Caroline Oliveira <carolioli@outlook.com>
2019-05-14 11:54:48 -0300

    Update - Adiciona Activity para recuperar senha e declara os Ids dos objetos da classe LoginActivity

--- app/src/main/java/com/conformidade/petrobras/apco/LoginActivity.java
+++ app/src/main/java/com/conformidade/petrobras/apco/LoginActivity.java
@@ -1,13 +1,29 @@
 package com.conformidade.petrobras.apco;
 
+import android.support.design.widget.TextInputEditText;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class LoginActivity extends AppCompatActivity {
+
+    private TextInputEditText login;
+    private TextInputEditText senha;
+    private Button botaoEntrar;
+    private TextView linkCadastrar;
+    private TextView linkRecuperarSenha;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        login = findViewById(R.id.inputLogin);
+        senha = findViewById(R.id.inputPassword);
+        botaoEntrar = findViewById(R.id.btnEntrar);
+        linkCadastrar = findViewById(R.id.linkCadastro);
+        linkRecuperarSenha = findViewById(R.id.linkSenha);
+
     }
 }
