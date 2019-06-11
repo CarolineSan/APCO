f11893729477cca5ca9bd064bfee74af451cba49
CarolineSan <carolioli@outlook.com>
2019-06-11 16:48:01 -0300

    TabLayout de Categorias personalizado com Scrollmode

--- app/src/main/java/com/conformidade/petrobras/apco/categoria/CategoriaActivity.java
+++ app/src/main/java/com/conformidade/petrobras/apco/categoria/CategoriaActivity.java
@@ -7,11 +7,12 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
 import com.conformidade.petrobras.apco.R;
+import com.conformidade.petrobras.apco.helper.CustomTabLayout;
 
 public class CategoriaActivity extends AppCompatActivity implements Tab1.OnFragmentInteractionListener, Tab2.OnFragmentInteractionListener, Tab3.OnFragmentInteractionListener {
 
     private android.support.v7.widget.Toolbar toolbar;
-    private TabLayout tabLayout;
+    private CustomTabLayout tabLayout;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -27,28 +28,29 @@ public class CategoriaActivity extends AppCompatActivity implements Tab1.OnFragm
 
         //Configura Menu / TabLayout
         tabLayout = findViewById(R.id.tabLayout);
+        tabLayout.addTab(tabLayout.newTab().setIcon(R.drawable.ic_home_white_24dp));
         tabLayout.addTab(tabLayout.newTab().setText("Menu 1"));
         tabLayout.addTab(tabLayout.newTab().setText("Menu 2"));
         tabLayout.addTab(tabLayout.newTab().setText("Menu 3"));
-        tabLayout.setTabGravity(TabLayout.GRAVITY_FILL);
+        tabLayout.addTab(tabLayout.newTab().setText("Menu 4"));
 
-        final ViewPager viewPager = findViewById(R.id.viewPager);
-        final PagerAdapter adapter = new PagerAdapter(getSupportFragmentManager(), tabLayout.getTabCount());
-        viewPager.setAdapter(adapter);
-        viewPager.setOnPageChangeListener(new TabLayout.TabLayoutOnPageChangeListener(tabLayout));
-        tabLayout.setOnTabSelectedListener(new TabLayout.OnTabSelectedListener() {
+        //final ViewPager viewPager = findViewById(R.id.viewPager);
+        //final PagerAdapter adapter = new PagerAdapter(getSupportFragmentManager(), tabLayout.getTabCount());
+        //viewPager.setAdapter(adapter);
+        //viewPager.setOnPageChangeListener(new TabLayout.TabLayoutOnPageChangeListener(tabLayout));
+        tabLayout.setOnTabSelectedListener(new CustomTabLayout.OnTabSelectedListener() {
             @Override
-            public void onTabSelected(TabLayout.Tab tab) {
-                viewPager.setCurrentItem(tab.getPosition());
+            public void onTabSelected(CustomTabLayout.Tab tab) {
+                //int totalTabs = tabLayout.getTabCount();
             }
 
             @Override
-            public void onTabUnselected(TabLayout.Tab tab) {
+            public void onTabUnselected(CustomTabLayout.Tab tab) {
 
             }
 
             @Override
-            public void onTabReselected(TabLayout.Tab tab) {
+            public void onTabReselected(CustomTabLayout.Tab tab) {
 
             }
         });
